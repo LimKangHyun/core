8f4ba8b379d2824a3ffb3bd719ad8db12ae2f260
limkanghyun <limkh06910691@gmail.com>
2024-08-16 01:04:15 +0900

    DIP(의존성 관계 주입) 작업 AppConfig를 통해 객체의 생성과 연결을 담당

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,20 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+//객체의 생성과 연결 담당
+public class AppConfig {//실제 동작에 필요한 구현 객체를 생성
+
+    public MemberService memberService() {//생성자 주입
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+}
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -11,8 +11,9 @@ import hello.core.order.OrderServiceImpl;
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
@@ -20,6 +21,6 @@ public class OrderApp {
 
         Order order = orderService.createOrder(memberId, "itemA", 10000);
         System.out.println("order = " + order.toString());
-        System.out.println("order.calculatePrice = " + order.calculatePrice());
+//        System.out.println("order.calculatePrice = " + order.calculatePrice());
     }
 }
