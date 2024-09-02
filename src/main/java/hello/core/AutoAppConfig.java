6da223a26379d36a7571772fa3ed33d3c23a820c
limkanghyun <limkh06910691@gmail.com>
2024-09-02 18:06:20 +0900

    컴포넌트 스캔, 기본 스캔대상, 필터, 중복 등록과 충돌

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,14 +1,23 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
+        excludeFilters= @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )//수동으로 등록한 bean을 자동등록 방지 (중복방지)
 
 public class AutoAppConfig {
 
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
